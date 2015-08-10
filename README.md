ea191f41064219481386a673b0433315eb73fbaa
Austin Ankney <codebeatstode@aol.com>
2015-08-09 21:46:45 -0400

    updated readme and added script

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # tempy
+
+Tempy is a simple, single file python script which makes it easy to modularize html files for static web pages. This makes it easier to edit content which is displayed on more than one page in a sight like a header or footer.
+
+## Setup
+
+Tempy is has three different directories that you'll need to understand.
+
+1. Templates
+2. Modules
+3. Production
+
+
+### Templates
+
+This directory is for files that import code from the modular files. For example, and index.html which uses a header and footer would go in this directory. To utilize a module from a template, simply insert a module tag `[[[module_tag]]]` in a template file. The module tag must show the same name as the file being called from the modules directory. An example of this is below.
+
+Here, an index file inserts a header from the content in /modules/header.html and a footer from /modules/footer.html
+
+```
+index.html
+
+<html>
+  <body>
+    <!-- insert header -->
+    [[[header]]]
+
+    <p> normal webpage content... <p>
+
+    <!-- insert footer -->
+    [[[footer]]]
+  </body>
+</html>
+```
+
+### Modules
+
+Modules are files containing code which are then inserted into files in the templates directory. This is where you would want to put header.html and footer.html
+
+### Production
+
+The production directory contains the final html files with the module content inserted into the template pages. The idea is that any files in the production directory are ready to be pushed to your web server. 
+
---
+++ temp.py
@@ -0,0 +1,82 @@
+'''
+Tempy script
+
+Author: Austin Ankney
+Date: 8/7/15
+Description: A simple script to help manage html files
+    in a modular way to maximize code reuse while avoiding
+    javascript alternatives.
+'''
+
+import os
+import re
+
+class tempy(object):
+    def __init__(self):
+        self.base_dir = os.path.dirname(os.path.realpath(__file__))
+        self.template_files = self._getFiles(self.base_dir + "/templates")
+        self.module_files = self._getFiles(self.base_dir + "/modules")
+        self.modules = self._getModules()
+
+    def _getFiles(self,dir_path):
+        files = []
+        for dirpath, dirnames, filenames in os.walk(dir_path):
+            if len(filenames) > 0:
+                for f in filenames:
+                    file_path = dirpath + "/" + f
+                    files.append(file_path)
+
+        return files
+
+    def _getModules(self):
+        modules = {}
+        for f in self.module_files:
+            key_name = f.split("/")[-1].split(".")[0]
+            r = open(f, 'r')
+            content = r.read()
+
+            modules[key_name] = content
+
+        return modules
+
+    def _sub(self, f, template):
+        ## Extract only characters between [[[ and ]]]
+        regex = re.compile(r'\[\[\[(\w+)\]\]\]')
+
+        ## Only extract contents when the parse symbol is present
+        if regex.search(template) is not None:
+
+            ## Extract parts of response that need replaced
+            replacers = regex.findall(template)
+
+            for part in replacers:
+                print "Replacing " + part + " in " + f
+                template = template.replace("[[[" + str(part) + "]]]", self.modules[str(part)])
+
+        new_template = template
+
+        return new_template
+
+
+    def produce(self):
+        for f in self.template_files:
+            production_file = f.split("/")[-1]
+            r = open(f, 'r')
+            temp_content = r.read()
+            production_content = self._sub(f, temp_content)
+            r.close()
+
+            print "Writing " + production_file + " to production"
+            w = open(self.base_dir + "/production/" + production_file, 'w')
+            w.write(production_content)
+            w.close()
+
+        print "Production complete"
+
+
+
+if __name__ == "__main__":
+    t = tempy()
+    t.produce()
+
+
